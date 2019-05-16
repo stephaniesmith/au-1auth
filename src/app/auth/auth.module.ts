2fbcf360d3d67279c0a8253c5a68831e8494fb62
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-05-16 08:36:03 -0700

    cleanup

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,13 +1,13 @@
-import {ModuleWithProviders, NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {LoginComponent} from './login/login.component';
-import {MatCardModule} from '@angular/material/card';
-import {MatInputModule} from '@angular/material';
-import {RouterModule} from '@angular/router';
-import {ReactiveFormsModule} from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
-import { StoreModule } from '@ngrx/store';
-import {AuthService} from './auth.service';
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { StoreModule  } from '@ngrx/store';
+import { AuthService } from './auth.service';
 import * as fromAuth from './auth.reducer';
 
 
