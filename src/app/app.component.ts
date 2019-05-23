083c2274f789c189326da79368910a5d7bb01a59
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-05-23 08:09:23 -0700

    working on effects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,6 +30,5 @@ export class AppComponent implements OnInit {
 
   logout() {
     this.store.dispatch(new Logout());
-    this.router.navigateByUrl('/login');
   }
 }
--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
-import { Actions, Effect } from '@ngrx/effects';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Login, AuthActionTypes } from './auth.actions';
+import { tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 
 @Injectable()
 export class AuthEffects {
 
-  constructor(private actions$: Actions) {}
+  @Effect({ dispatch: false })
+  login$ = this.actions$.pipe(
+    ofType<Login> (AuthActionTypes.LoginAction),
+    tap(({ payload }) => localStorage.setItem('user', JSON.stringify(payload)))
+  );
+
+  @Effect({ dispatch: false })
+  logout$ = this.actions$.pipe(
+    ofType<Login> (AuthActionTypes.LogoutAction),
+    tap(() => {
+      localStorage.removeItem('user');
+      this.router.navigateByUrl('/login');
+    })
+  );
+
+  constructor(
+    private actions$: Actions,
+    private router: Router
+  ) {}
 }
